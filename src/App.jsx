90dfeff62eb39e07dadf72657848b14c0afc7724
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-19 20:56:11 +0100

    add navbar feature for registering plate

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Navbar from './components/Navbar/Navbar'
+import RegisterPlate from './pages/RegisterPlate'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/register-plate" element={<RegisterPlate/>}/>
     </Routes>
     </>
   )
