d8b67eee6b8089824257be0a3e493fab00b6f990
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-19 20:18:14 +0100

    create navbar folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
     </Routes>
