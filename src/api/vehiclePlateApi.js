2aeb7210051fe842d0527c6226df8c4c59e4b3e3
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-20 19:33:10 +0100

    register vehicle post created

--- src/api/vehiclePlateApi.js
+++ src/api/vehiclePlateApi.js
@@ -2,4 +2,4 @@ import axiosInstance from './axiosInstance';
 
 export const getVehiclePlatesByInput = (input) => axiosInstance.get(`/vehiclePlates/reg/${input}`)
 
-export const createVehicle = (body) => axiosInstance.post('/vehicles', body)
+export const createVehiclePlate = (body) => axiosInstance.post('/vehiclePlates', body)
--- src/pages/RegisterPlatePage.jsx
+++ src/pages/RegisterPlatePage.jsx
@@ -1,79 +1,104 @@
-import React from 'react'
-import StandardRegPlateInput from '../components/StandardRegPlateInput';
-import { useRef, useState } from 'react';
-import '../assets/button.css'
-
+import React from "react";
+import StandardRegPlateInput from "../components/StandardRegPlateInput";
+import { useRef, useState } from "react";
+import "../assets/button.css";
+import { createVehiclePlate } from "../api/vehiclePlateApi";
+// import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
 
 
 const RegisterPlatePage = () => {
-    const [values, setValues] = useState(['', '', '']);
-    const [errors, setErrors] = useState([false, false, false]);  
-    const regplateProps = {values, setValues, errors, setErrors}
-    const [price, setPrice] = useState(0)
-    const [available, setAvailable] = useState(true)
-    let strErrMessage = "Letters only accepted"
-    let numErrMessage = "Numbers only accepted"
+  const [values, setValues] = useState(["", "", ""]);
+  const [errors, setErrors] = useState([false, false, false]);
+  const regplateProps = { values, setValues, errors, setErrors };
+  const [price, setPrice] = useState(0);
+  const [available, setAvailable] = useState(true);
+  let strErrMessage = "Letters only accepted";
+  let numErrMessage = "Numbers only accepted";
 
+  const renderErrors = () => {
+    if (!errors.some((err) => err)) return null;
+    return (
+      <>
+        <h3>Error Found:</h3>
+        {errors[1] ? <li>{numErrMessage}</li> : <li>{strErrMessage}</li>}
+      </>
+    );
+  };
 
-    const renderErrors = () => {
-      if (!errors.some(err => err)) return null;
-    
-      return (
-        <>
-          <h3>Error Found:</h3>
-          {errors[1] ? (
-            <li>{numErrMessage}</li>
-          ) : (
-            <li>{strErrMessage}</li>
-          )}
-        </>
-      );
-    };
+  const handlePriceInput = (e) => {
+    const value = e.target.value;
 
-    const handlePriceInput = (e) => {
-      const value = e.target.value;
-
-      if (isNaN(value) || value === "") {
-        return; // If it's not a number, don't update the state
-      }
-    
-    
-        setPrice(value); // Allow empty input or positive numbers
-      
+    if (isNaN(value) || !value) {
+      return;
     }
-    
+    setPrice(value); // Allow empty input or positive numbers
+  };
 
+  const handleRegisterStandardPlate = (e) => {
+    e.preventDefault();
+    const body = {
+      plateNumber: values.join('').trim(),
+      personalised: false,
+      available: available,
+      price: Number(price),
+      customerId: null
+    }
+    createVehiclePlate(body)
+    .then(res => {
+      console.log("Vehicle plate created:", res);
+      toast.success("New vehicle plate registered!")
+      setValues(["","",""])
+      setPrice(0)
+      setAvailable(true)
+      setErrors([false, false, false])
 
- 
-  return(
+    })
+    .catch(e => {
+      console.log(e)
+      toast.error("Vehicle plate not registered")
+  })
+  };
+
+  return (
     <>
-      
-     <h3>Register a new pate - Standard</h3>
-  
+      <h3>Register a new pate - Standard</h3>
+
       {renderErrors()}
-   
-    <form>
-    <div className = "form-group">
-    <label className = "inline">Registration number:</label>
-    <StandardRegPlateInput props={regplateProps}/>
-    </div>
-    <div className ="form-group">
-      <label htmlFor="plate-price">Price:</label>
-      <input name="plate-price"  type="text" value={price} onChange={handlePriceInput} />
-    </div>
-    <div className="form-group">
-      <label htmlFor="">Available: </label>
-    <label className="switch">
-          <input type="checkbox" checked={available} onChange={(e) =>setAvailable(e.target.checked)}/>
+
+      <form onSubmit={handleRegisterStandardPlate}>
+        <div className="form-group">
+          <label className="inline">Registration number:</label>
+          <StandardRegPlateInput props={regplateProps} />
+        </div>
+        <div className="form-group">
+          <label>Price:</label>
+          <input
+            name="plate-price"
+            type="text"
+            value={price}
+            onChange={handlePriceInput}
+          />
+        </div>
+        <div className="form-group">
+          <label>Available: </label>
+          <label className="switch">
+            <input
+              type="checkbox"
+              checked={available}
+              onChange={(e) => setAvailable(e.target.checked)}
+            />
             <span className="slider round"></span>
           </label>
-    </div>
-
-    
-    </form>
+        </div>
+        {!errors.some((err) => err) ? (
+          <button>Register</button>
+        ) : (
+          <button disabled>Register</button>
+        )}
+      </form>
     </>
-  )
-  
-}
+  );
+};
 
-export default RegisterPlatePage
+export default RegisterPlatePage;
