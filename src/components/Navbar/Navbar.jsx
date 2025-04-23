c10d0f00c21d845528fc1eadc0d1ec2d20356e61
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-04-23 18:27:00 +0100

    create auth:

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React, {useState} from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 
 const Navbar = () => {
   const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
+  const {user} = useAuth();
+  const isAdmin = user?.role === 'ADMIN'
+  const isCustomer = user?.role === 'CUSTOMER'
+  const isLoggedIn = user !== null;
+    //  const isAdmin = true
 
   const toggleNavbar = () => {
     setIsNavbarCollapsed(!isNavbarCollapsed);
@@ -30,12 +36,18 @@ const Navbar = () => {
           <li className="nav-item active">
           <Link className="nav-link" to="/">Home</Link>
           </li>
+          {isAdmin &&(
+            <>
           <li className="nav-item">
           <Link className="nav-link" to="/register-plate">Register Plate </Link>
           </li>
+          
           <li className="nav-item">
             <Link className="nav-link" to="/transactions">Transactions</Link>
           </li>
+          </>
+          )
+        }
         </ul>
         {/* <span className="navbar-text">
           Navbar text with an inline element
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 )
