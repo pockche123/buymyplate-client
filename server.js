33ba3a25091d0a795145b6b05ecefdfd53d23f94
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-05-09 19:42:50 +0100

    setup docker

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = 5173;
 
 // Serve static files from 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
